25cfb9b0f2bd40b4b180e42c91fa4b74cad095c7
Altaks <57632793+Altaks@users.noreply.github.com>
2022-05-19 22:59:32 +0200

    research extension working

--- interface/js/popup.js
+++ interface/js/popup.js
@@ -1,20 +1,48 @@
 const searchInput = document.getElementById("search");
 const rawSearchInput = document.getElementById("raw-search");
 const extensionInput = document.getElementById("extension");
-const websiteInput = document.getElementById("website-search");
+const websiteInput = document.getElementById("website");
 
 const dateBeginInput = document.getElementById("date-beginning");
 const dateEndInput = document.getElementById("date-ending");
 
-const submitButton = document.getElementById("submitButton");
+const submitButton = document.getElementById("submit");
 
-submitButton.addEventListener("click", function(event) {
+submitButton.onclick = function(event) {
 
-    var link = "";
+    var link = "https://www.google.com/search?q=";
 
     // add calculations here
+    if(searchInput.value != "") {
+        if(rawSearchInput.checked) {
+            link += "\""
+        }
+        link += searchInput.value;
+        if(rawSearchInput.checked) {
+            link += "\""
+        }
+    }
 
+    if(extensionInput.value != "") {
+        link += "+ext:" + extensionInput.value;
+    }
 
-    window.open(link);
+    if(websiteInput.value != "") {
+        link += "+website:" + websiteInput.value;
+    }
 
-});
+    if(dateBeginInput.value != "" && dateEndInput.value != "") {
+
+        Date.prototype.getJulian = function() {
+            return Math.ceil((this / 86400000) - (this.getTimezoneOffset()/1440) + 2440587.5);
+        }
+        
+        var dateBegin = new Date(dateBeginInput.value);
+        var dateEnd = new Date(dateEndInput.value);
+
+        link += "+" + dateBegin.getJulian() + ".." + dateEnd.getJulian();
+    }
+
+    window.open(link, '_blank');
+
+}
