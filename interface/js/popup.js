e257671b8a5fabeb6ab133c98b4179cbcbef5a69
Altaks <57632793+Altaks@users.noreply.github.com>
2022-05-19 22:39:09 +0200

    preparing event listener

---
+++ interface/js/popup.js
@@ -0,0 +1,13 @@
+const searchInput = document.getElementById("search");
+const rawSearchInput = document.getElementById("raw-search");
+const extensionInput = document.getElementById("extension");
+const websiteInput = document.getElementById("website-search");
+
+const dateBeginInput = document.getElementById("date-beginning");
+const dateEndInput = document.getElementById("date-ending");
+
+const submitButton = document.getElementById("submitButton");
+
+submitButton.addEventListener("click", function(event) {
+    
+});
